92b8f439c9ed869aa947906262c1097ae889f8a9
shameer <shameer.khan734@gmail.com>
2026-04-15 11:08:21 +0530

    first

---
+++ Desktop/static-website/script.js
@@ -0,0 +1,14 @@
+// Show alert when page loads
+window.onload = function() {
+    console.log("Website loaded successfully 🚀");
+};
+
+// Function for button click
+function showMessage() {
+    alert("Hello! Your S3 static website is working 🎉");
+}
+
+// Change text dynamically
+function changeText() {
+    document.getElementById("demo").innerHTML = "Text changed using JavaScript!";
+}
